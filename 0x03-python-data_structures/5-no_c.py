e7503561caa4efe040bcf6009415dac64e6f4439
Purnuple <nndlovu426@gmail.com>
2022-12-03 07:04:07 -0800

    task5 changes

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_str = ""
-     for i in range(len(my_string)):
+    for i in range(len(my_string)):
         if my_string[i] == 'C' or my_string[i] == 'c':
             pass
         else:
